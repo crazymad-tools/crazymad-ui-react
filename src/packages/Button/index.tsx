8fac0b494c5c05360bcc6aa15975fa3bdcce0e04
crazy_mad <crazymad0601@gmail.com>
2019-09-29 00:57:02 +0800

    波纹按钮

--- src/packages/Button/index.tsx
+++ src/packages/Button/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import { EventInterface } from '../common/EventInterface';
 import "./index.scss";
 
 interface ButtonProps extends EventInterface {
   type?: String;
+  style?: any;
 }
 
 const Button: React.FC<ButtonProps> = props => {
@@ -12,19 +14,46 @@ const Button: React.FC<ButtonProps> = props => {
     ...event
   } = props;
 
+  const [wave, setWave] = useState<{
+    style: {
+      left: string,
+      top: string,
+      width: string,
+      height: string,
+      borderRadius: string,
+    },
+    id: number
+  }>();
+
   function onclick(e: any) {
-    if (props.onClick instanceof Function) {
-      props.onClick(e);
-    }
+    let x = e.nativeEvent.offsetX;
+    let y = e.nativeEvent.offsetY;
+    let width = e.currentTarget.clientWidth;
+    let height = e.currentTarget.clientHeight;
+    let wave = {
+      style: {
+        left: -width + x + 'px',
+        top: -height * 2 + y + 'px',
+        width: Math.max(width, height) * 2 + 'px',
+        height: Math.max(width, height) * 2 + 'px',
+        borderRadius: '100%'
+      },
+      id: new Date().getTime()
+    };
+    setWave(wave);
+    props.onClick && props.onClick(e);
   }
 
   return (
     <button
-      className={`plan-btn ${
-        props.type ? "plan-btn-" + props.type : "plan-btn-default"
+      style={props.style}
+      className={`cm-btn ${
+        props.type ? "cm-btn-" + props.type : "cm-btn-default"
       }`}
       {...event}
+      onMouseDown={onclick}
     >
+      {wave && <span className="cm-btn-wave" style={wave.style} key={wave.id}/>}
       {props.children}
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,34 +3,54 @@ import { useState } from "react";
 import { Dialog, Icon, Button } from "../packages";
 
 const IndexPage: React.FC = props => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState<any>({
+    demo1: false
+  });
 
-  function showDialog () {
-    setShow(true);
+  function showDialog(key: string) {
+    show[key] = true;
+    setShow(Object.assign({}, show));
   }
 
-  function hideDialog () {
-    setShow(false);
+  function hideDialog(key: string) {
+    show[key] = false;
+    setShow(Object.assign({}, show));
   }
 
   return (
     <div>
-      <Button onClick={showDialog} type="primary">OPEN</Button>
+      <Button onClick={showDialog.bind(null, 'demo1')} type="primary">DEMO1</Button>
+      <Button style={{ marginLeft: '20px' }} onClick={showDialog.bind(null, 'demo2')} type="primary">DEMO2</Button>
       <Dialog
-        title="demo"
-        visible={show}
-        close={hideDialog}
-        confirm={hideDialog}
-        cancel={hideDialog}
+        title="demo1"
+        visible={show.demo1}
+        close={hideDialog.bind(null, 'demo1')}
+        confirm={hideDialog.bind(null, 'demo1')}
+        cancel={hideDialog.bind(null, 'demo1')}
         drag={false}
-        offset={{top: '100px'}}
         mask={true}
       >
         <div>
           <h3>Dialog demo</h3>
-          <p>this is a dialog</p>
+          <p>默认居中，开启遮罩，不允许拖拽</p>
         </div>
       </Dialog>
+
+      <Dialog
+        title="demo2"
+        visible={show.demo2}
+        close={hideDialog.bind(null, 'demo2')}
+        confirm={hideDialog.bind(null, 'demo2')}
+        cancel={hideDialog.bind(null, 'demo2')}
+        offset={{ top: '100px' }}
+      >
+        <div>
+          <h3>Dialog demo</h3>
+          <p>左右居中，不开启遮罩，允许拖拽</p>
+        </div>
+      </Dialog>
+      <hr />
+      <Button>波纹</Button>
     </div>
   );
 };
